a0925487066df4959148bc08d1671d9681c1df34
Raphael Cavalcante da Silva <raphael.silva@fitbank.com.br>
2022-02-15 13:22:08 -0300

    Playing with exceptions

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Informe a opção desejada");
+Console.WriteLine("1 - Inserir novo aluno");
+Console.WriteLine("2 - Listar novo aluno");
+Console.WriteLine("3 - Calcular média geral");
+Console.WriteLine("");
+
+string optionSelected = Console.ReadLine();
+
+try
+{
+    if (String.IsNullOrEmpty(optionSelected))
+    {
+        throw new ArgumentException("Opção não selecionada");
+    };
+}catch(ArgumentException error){
+  Console.Write(error);
+}
+
